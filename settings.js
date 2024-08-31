e2d2bcb1106a939f912acd56e504ace633c70874
nouraj7 <akida.bff@gmail.com>
2024-08-31 16:21:48 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
 global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.MONGO_DB_URI = "mongodb+srv://sunlight:sunlight@cluster0.kcqknz1.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "bk9.dev@gmail.com" //email
+global.email_password = "ufvh qtdf lbgl yavk" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
